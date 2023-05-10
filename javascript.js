e2dfb7985181c18c953dd0b161b4465f98e1367a
ynechaiev <85703497+ynechaiev@users.noreply.github.com>
2023-05-10 15:45:20 -0700

    First function

---
+++ javascript.js
@@ -0,0 +1,13 @@
+console.log("======================");
+
+function getComputerChoice(animal) {
+    return animal + " is my favorite animal!"
+  }
+ 
+  console.log(favoriteAnimal('Goat'));
+  
+  console.log(getComputerChoice());
+
+
+
+  console.log("======================");
