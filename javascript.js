f371f5d04b57744647662ad9ddda34be969282b2
ynechaiev <85703497+ynechaiev@users.noreply.github.com>
2023-05-10 15:46:46 -0700

    test

--- javascript.js
+++ javascript.js
@@ -1,13 +1,19 @@
 console.log("======================");
 
-function getComputerChoice(animal) {
-    return animal + " is my favorite animal!"
-  }
+let maxNum = 0;
+
+function getComputerChoice(maxNum) {
+    
+    let randomNumber = Math.floor(Math.random() * maxNum);
+    
+    return randomNumber;
+
+};
  
-  console.log(favoriteAnimal('Goat'));
-  
-  console.log(getComputerChoice());
+console.log(getComputerChoice(3));
+console.log(getComputerChoice(3));
+console.log(getComputerChoice(3));
 
 
 
-  console.log("======================");
+console.log("======================");
